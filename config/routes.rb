faeb36aecc7485172a9db53c0956e93660bb9c9f
min <min>
2026-04-06 04:17:34 +0900

    closes #8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   # トップページの設定
   root "font_designs#index"
+
+  # ログイン/ログアウト用(次のステップで必要)
+  get 'login', to: 'sessions#new'
 end

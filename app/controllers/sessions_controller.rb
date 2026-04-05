6f688e0250c396e96553befbd25f4a688b47c1ab
min <min>
2026-04-06 04:38:21 +0900

    closes #8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
-  def new
-  end
+def new
+end
 
-  def create
+def create
+  user = login(params[:email], params[:password])
+  
+  if user
+    redirect_to root_path, notice: 'ログインしました'
+  else
+    flash.now[:alert] = 'ログインに失敗しました'
+    render :new
   end
+end
 
-  def destroy
-  end
+def destroy
+  logout
+  redirect_to root_path, notice: 'ログアウトしました', status: :see_other
+end
 end

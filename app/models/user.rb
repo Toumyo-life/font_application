8902f5c6fff76c3bfd1767a8c9e336d387f06b25
min <min>
2026-04-06 17:01:42 +0900

    closes #9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :email, presence: true, uniqueness: true
+
+  has_many :font_designs, dependent: :destroy
+  has_one_attached :avatar
+
+  def full_name
+    "#{last_name} #{first_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
+  get "users/new"
+  get "users/create"
+  # ログイン/ログアウト用(次のステップで必要)
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
   # トップページの設定
   root "font_designs#index"
+
+  # ユーザー登録用
+  resources :users, only: [:new, :create]
 
   # ログイン/ログアウト用(次のステップで必要)
   get "login", to: "sessions#new"

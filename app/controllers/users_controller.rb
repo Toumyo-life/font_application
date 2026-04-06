a39998789ec1876b558496bd4da57d69ed226ead
min <min>
2026-04-06 22:35:45 +0900

    Closes #8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,18 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to login_path, notice: "#{@user.first_name}さん、ユーザー登録が完了しました"
+      redirect_to sessions_new_path
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+  end
+end
 
   private
 
